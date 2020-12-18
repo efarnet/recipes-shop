f46f6459042dc449b4b330b2cd9e8ec02310dca7
Eric <rice069@gmail.com>
2020-12-18 18:00:20 +0100

    fix form control import

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/Formcontrol';
+import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 // import {Link} from 'react-router-dom';
